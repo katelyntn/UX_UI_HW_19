c23d4d9ce6397bcce2799e683f0445f02865d4f5
katelyntn <nguyenkatelyn1210@gmail.com>
2021-06-13 18:35:50 -0700

    Updated

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$('.dlresumebutton').on("click", function() {
+    alert("Resume downloaded");
+});
+
+$('.myworkimg').hover(function(){
+    $(this).width(1.03*$(this).width());
+    $(this).height(1.03*$(this).height());
+}, function(){
+    $(this).width(0.97*$(this).width());
+    $(this).height(.97*$(this).height());
+
+    console.log("mouse leave");
+});
+
+
+$('.nav').on('click', function(){
+    console.log("clicked");
+    $('html, body').animate(
+        {
+        scrollTop: $(hash).offset().top
+    },
+    800
+    );
+});
